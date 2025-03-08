137f22de5bfcf3e5983896638d081e924f25f93a
dimasiggs <dima.dashkin@bk.ru>
2025-03-09 01:10:11 +0500

    build: Добавил контейнеризацию Docker

---
+++ code/frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h2>Welcome to Eventify!</h2>
+        <a>The best app in th WORLD!</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
